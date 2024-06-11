a1e51a3717d4d4b6d95cbb94f2be4c8f65114cc3
Rakha <rakharsya.fatra@gmail.com>
2024-06-11 23:37:48 +0700

    rakha

--- program10.2.1.php
+++ program10.2.1.php
@@ -1,25 +1,25 @@
 <?php
 $nilai = 78;
 if ($nilai >= 80 && $nilai <= 100) {
-    echo "$nilai mendapatkan nilai angka A";
+    echo "$nilai mendapatkan nilai huruf A";
 } else if ($nilai >= 76.25 && $nilai <= 79.99) {
-    echo "$nilai mendapatkan nilai angka A-";
+    echo "$nilai mendapatkan nilai huruf A-";
 } else if ($nilai >= 68.75 && $nilai <= 76.24) {
-    echo "$nilai mendapatkan nilai angka B+";
+    echo "$nilai mendapatkan nilai huruf B+";
 } else if ($nilai >= 65 && $nilai <= 68.74) {
-    echo "$nilai mendapatkan nilai angka B";
+    echo "$nilai mendapatkan nilai huruf B";
 } else if ($nilai >= 62.50 && $nilai <= 64.99) {
-    echo "$nilai mendapatkan nilai angka B-";
+    echo "$nilai mendapatkan nilai huruf B-";
 } else if ($nilai >= 57.50 && $nilai <= 62.49) {
-    echo "$nilai mendapatkan nilai angka C+";
+    echo "$nilai mendapatkan nilai huruf C+";
 } else if ($nilai >= 55 && $nilai <= 57.99) {
-    echo "$nilai mendapatkan nilai angka C";
+    echo "$nilai mendapatkan nilai huruf C";
 } else if ($nilai >= 51.25 && $nilai <= 54.49) {
-    echo "$nilai mendapatkan nilai angka C-";
+    echo "$nilai mendapatkan nilai huruf C-";
 } else if ($nilai >= 43.75 && $nilai <= 51.24) {
-    echo "$nilai mendapatkan nilai angka D+";
+    echo "$nilai mendapatkan nilai huruf D+";
 } else if ($nilai >= 40 && $nilai <= 43.74) {
-    echo "$nilai mendapatkan nilai angka D";
+    echo "$nilai mendapatkan nilai huruf D";
 } else if ($nilai >= 0 && $nilai <= 39.99) {
-    echo "$nilai mendapatkan nilai angka E";
+    echo "$nilai mendapatkan nilai huruf E";
 }
